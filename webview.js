05bcbef33ce256d814813fc4860353b412d321d4
Alex Devero <deveroalex@gmail.com>
2020-01-24 16:33:12 +0100

    Add script to hide promoted links

--- webview.js
+++ webview.js
@@ -3,6 +3,12 @@ var _path = _interopRequireDefault(require("path"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 module.exports = (Franz) => {
+  if (document.querySelectorAll('.promotedlink').length > 0) {
+    document.querySelectorAll('.promotedlink').forEach((sponsoredLink) => {
+      sponsoredLink.parentElement.parentElement.style.display = 'none'
+    })
+  }
+
   // Use CSS to hide Google Ads
   Franz.injectCSS(_path.default.join(__dirname, 'service.css'))
 }
